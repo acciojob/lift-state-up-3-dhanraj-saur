be4e03c2d23658d4257f80887dbba9eaf5427ee0
dhanraj-saur <dhananjayverma301@gmail.com>
2023-07-12 13:21:14 +0530

    done1

--- src/components/Child2.js
+++ src/components/Child2.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Child2({setSelectedOption}) {
     return (
       <div className='child2'>
+          <h3>Child Component 2</h3>
         <button onClick={(e) => setSelectedOption(e.target.textContent)}>Option 2</button>
       </div>
     )
